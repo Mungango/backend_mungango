59b02d35a477df13717eee8fa802bbff3f0d157b
peth <pethersonreis@gmail.com>
2024-04-19 21:49:03 -0300

    fix: postUserSchema

--- src/services/posts/getPosts.service.js
+++ src/services/posts/getPosts.service.js
@@ -1,28 +1,29 @@
 import { AppError } from "../../errors.js";
 import User from "../../models/User.js";
 import Post from "../../models/Post.js";
-import { usersUpdateSchema, usersWithoutPassSchema } from "../../schema/users.schema.js";
+import {
+	usersUpdateSchema,
+	usersWithoutPassSchema,
+} from "../../schema/users.schema.js";
+import { postUserSchema } from "../../schema/posts.schema.js";
 
 const getPostsService = async (id) => {
-  const retrivedPost = await Post.findOne({
-    where: { id },
-  });
-  console.log(retrivedPost);
+	const retrivedPost = await Post.findOne({
+		where: { id },
+		include: [
+			{
+				model: User,
+        where: {deletedAt: null}
+			},
+		],
+	});
+  
+  // quando o usuário estiver soft deletado isso vai igual a true
+  if (!retrivedPost){
+    throw new AppError("Post não encontrado", 404)
+  }
 
-  const userId = retrivedPost.UserId;
-
-  const userData = await User.findOne({ where: { id: userId } });
-
-  const userWithoutPass = usersWithoutPassSchema.parse(userData);
-
-  const postWithUser = {
-    ...retrivedPost.dataValues,
-    user: userWithoutPass,
-  };
-
-  console.log(postWithUser);
-
-  return postWithUser;
+	return postUserSchema.parse(retrivedPost);
 };
 
 export default getPostsService;
