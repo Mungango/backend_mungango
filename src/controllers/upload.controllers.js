3a3d7608c0a3791f430182bad33b21e37fc73954
isabelle9912 <isabellesaahirah28@gmail.com>
2024-04-22 16:08:09 -0300

    fix: resolvendo conflito de branch

--- src/controllers/upload.controllers.js
+++ src/controllers/upload.controllers.js
@@ -4,20 +4,20 @@ import uploadService from "../services/upload.service.js";
 import { uploadWithoutIdSchema } from "../schemas/upload.schema.js";
 
 const saveImage = async (path) => {
-  const upload = await cloudinary.uploader.upload(
-    path,
-    (error, result) => result
-  );
-  // cloudinary.uploader.upload é o método que utilizamos para o upload
-  // req.file.path é o caminho da nossa imagem salva pelo multer na pasta upload
-  fs.unlink(path, (error) => {
-    if (error) {
-      console.log(error);
-    }
-  });
-  //fs é uma lib nativa do node.js para manipulação do sistema operacional
-  //fs.unlink esta apagando o arquivo da pasta upload após o envio ao cloudinary
-  return upload;
+	const upload = await cloudinary.uploader.upload(
+		path,
+		(error, result) => result
+	);
+	// cloudinary.uploader.upload é o método que utilizamos para o upload
+	// req.file.path é o caminho da nossa imagem salva pelo multer na pasta upload
+	fs.unlink(path, (error) => {
+		if (error) {
+			console.log(error);
+		}
+	});
+	//fs é uma lib nativa do node.js para manipulação do sistema operacional
+	//fs.unlink esta apagando o arquivo da pasta upload após o envio ao cloudinary
+	return upload;
 };
 
 const getImageController = async (req, res) => {
@@ -28,9 +28,9 @@ const getImageController = async (req, res) => {
 };
 
 const uploadImagePostController = async (req, res) => {
-  const upload = await saveImage(req.file.path);
+	const upload = await saveImage(req.file.path);
 
-  const postId = req.params.id;
+	const postId = req.params.id;
 
   const uploadedImage = await uploadService(postId, upload);
 
@@ -38,16 +38,16 @@ const uploadImagePostController = async (req, res) => {
 };
 
 const uploadImageController = async (req, res) => {
-  const upload = await saveImage(req.file.path);
+	const upload = await saveImage(req.file.path);
 
-  const parsedUpload = uploadWithoutIdSchema.parse({
-    publicId: upload.public_id,
-    secureUrl: upload.secure_url,
-    createdAt: new Date(upload.created_at),
-    ...upload,
-  });
+	const parsedUpload = uploadWithoutIdSchema.parse({
+		publicId: upload.public_id,
+		secureUrl: upload.secure_url,
+		createdAt: new Date(upload.created_at),
+		...upload,
+	});
 
-  return res.status(200).json(parsedUpload);
+	return res.status(200).json(parsedUpload);
 };
 
 export { getImageController, uploadImagePostController, uploadImageController };
