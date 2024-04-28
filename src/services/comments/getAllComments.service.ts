7006889625b2386c52bae99650e2bd0365e6f1da
isabelle9912 <isabellesaahirah28@gmail.com>
2024-04-28 10:57:51 -0300

    fix: model de comment

--- src/services/comments/getAllComments.service.ts
+++ src/services/comments/getAllComments.service.ts
@@ -2,27 +2,47 @@ import { iCommentUserNoUserId } from "../../interfaces/comment.interface";
 import Comment from "../../models/Comment";
 import Post from "../../models/Post";
 import User from "../../models/User";
+import LikesComment from "../../models/likesComment";
 import { commentUserNoUserIdSchema } from "../../schemas/comments.schema";
 
 const getAllCommentsService = async (
-	postId: number
+  postId: number
 ): Promise<iCommentUserNoUserId[]> => {
-	const retrivedComments = await Comment.findAll({
-		where: {
-			postId,
-		},
-		include: [
-			{
-				model: User,
-				where: { deletedAt: null },
-			},
-			{
-				model: Post,
-			},
-		],
-	});
+  const retrivedComments = await Comment.findAll({
+    where: {
+      postId,
+    },
+    include: [
+      {
+        model: User,
+        where: { deletedAt: null },
+      },
+      {
+        model: Post,
+      },
+    ],
+  });
 
-	return commentUserNoUserIdSchema.array().parse(retrivedComments);
+  // Para cada post, obtemos as contagens de likes e dislikes
+  const commentsWithLikesAndDislikes = await Promise.all(
+    retrivedComments.map(async (comment) => {
+      const like = await LikesComment.count({
+        where: { ownerId: comment.dataValues.id, type: "like" },
+      });
+
+      const dislike = await LikesComment.count({
+        where: { ownerId: comment.dataValues.id, type: "dislike" },
+      });
+
+      return {
+        ...comment.dataValues,
+        like,
+        dislike,
+      };
+    })
+  );
+
+  return commentUserNoUserIdSchema.array().parse(commentsWithLikesAndDislikes);
 };
 
 export default getAllCommentsService;
