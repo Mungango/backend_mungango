061f325082a0b3dcd5aecdd2598d56a4af3db998
peth <pethersonreis@gmail.com>
2024-05-06 11:56:31 -0300

    fix: pasta icone

--- src/routes/icon.routes.ts
+++ src/routes/icon.routes.ts
@@ -3,13 +3,13 @@ import {
 	uploadIconController,
 	uploadIconPostController,
 } from "../controllers/icon.controllers";
-import icon from "../middlewares/uploadIcon.middleware";
 import ensureExistsMiddleware from "../middlewares/ensureExists.middleware";
 import { Router } from "express";
 import User from "../models/User";
 import { v2 } from "cloudinary";
 import ensureIsAdminMiddleware from "../middlewares/ensureIsAdminMiddleware";
 import ensureTokenIsValidMiddleware from "../middlewares/ensureTokenIsValid.middleware";
+import upload from "../middlewares/uploadImage.middleware";
 
 const iconRoutes: Router = Router();
 
@@ -17,14 +17,14 @@ iconRoutes.get("/:public_id", getIconController); // exibir um icon
 
 iconRoutes.post(
 	"/:id",
-	icon.single("icon"),
+	upload.single("icon"),
 	ensureTokenIsValidMiddleware,
 	ensureIsAdminMiddleware,
 	ensureExistsMiddleware(User, "Usuário"),
 	uploadIconPostController
 ); // criar um icone
 
-iconRoutes.post("", icon.single("icon"), uploadIconController);
+iconRoutes.post("", upload.single("icon"), uploadIconController);
 
 iconRoutes.delete("/:resource_type", async (req, res) => {
 	await v2.uploader.destroy(req.params.resource_type);
