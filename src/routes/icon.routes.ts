f59e0cc132e63d98989c66d6fc9a2aa59cd499c4
peth <pethersonreis@gmail.com>
2024-05-06 11:53:48 -0300

    fix: comment routes

--- src/routes/icon.routes.ts
+++ src/routes/icon.routes.ts
@@ -1,31 +1,35 @@
 import {
-  getIconController,
-  uploadIconController,
-  uploadIconPostController,
+	getIconController,
+	uploadIconController,
+	uploadIconPostController,
 } from "../controllers/icon.controllers";
 import icon from "../middlewares/uploadIcon.middleware";
 import ensureExistsMiddleware from "../middlewares/ensureExists.middleware";
 import { Router } from "express";
 import User from "../models/User";
 import { v2 } from "cloudinary";
+import ensureIsAdminMiddleware from "../middlewares/ensureIsAdminMiddleware";
+import ensureTokenIsValidMiddleware from "../middlewares/ensureTokenIsValid.middleware";
 
 const iconRoutes: Router = Router();
 
 iconRoutes.get("/:public_id", getIconController); // exibir um icon
 
 iconRoutes.post(
-  "/:id",
-  icon.single("icon"),
-  ensureExistsMiddleware(User, "Usuário"),
-  uploadIconPostController
+	"/:id",
+	icon.single("icon"),
+	ensureTokenIsValidMiddleware,
+	ensureIsAdminMiddleware,
+	ensureExistsMiddleware(User, "Usuário"),
+	uploadIconPostController
 ); // criar um icone
 
 iconRoutes.post("", icon.single("icon"), uploadIconController);
 
 iconRoutes.delete("/:resource_type", async (req, res) => {
-  await v2.uploader.destroy(req.params.resource_type);
+	await v2.uploader.destroy(req.params.resource_type);
 
-  return res.status(204).send();
+	return res.status(204).send();
 });
 
 export default iconRoutes;
