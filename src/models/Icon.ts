037296f40a60686d32b2fe13f89130b24ae2fa41
peth <pethersonreis@gmail.com>
2024-06-18 23:41:26 -0300

    fix: icone relations

--- src/models/Icon.ts
+++ src/models/Icon.ts
@@ -2,6 +2,7 @@ import { DataTypes, Model } from "sequelize";
 
 import sequelize from "../db/connect";
 import { iIcon, iIconCreate } from "../interfaces/icon.interface";
+import User from "./User";
 
 class Icon extends Model<iIcon, iIconCreate> {
 	declare id: number;
@@ -37,5 +38,15 @@ Icon.init(
 // Fazendo a relação de um para muitos
 // Icon.belongsTo(User, { foreignKey: "userId" });
 // User.hasMany(Icon, { foreignKey: "userId" });
+// Fazendo a relação de um para muitos
+Icon.belongsToMany(User, {
+	through: "UserIcon",
+	foreignKey: "iconId",
+});
+
+User.belongsToMany(Icon, {
+	through: "UserIcon",
+	foreignKey: "userId",
+});
 
 export default Icon;
