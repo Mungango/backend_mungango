1f327b4346a872c4c4077227b61eb69092f710d2
peth <pethersonreis@gmail.com>
2024-06-29 17:50:58 -0300

    fix:  return getUrserAllPostsService

--- src/services/posts/getUserAllPosts.service.ts
+++ src/services/posts/getUserAllPosts.service.ts
@@ -9,58 +9,56 @@ import { iPostUserImageLike } from "../../interfaces/post.interface";
 import { raw } from "express";
 
 const getUserAllPostsService = async (
-  id: number,
-  page: number,
-  limit: number
+	id: number,
+	page: number,
+	limit: number
 ): Promise<iPostUserImageLike> => {
-  const offset = (page - 1) * limit;
-  const retrivedPosts = await Post.findAll({
-    limit,
-    offset,
-    include: [
-      {
-        model: User,
-        where: { deletedAt: null, id },
-      },
-      { model: Image }, //inscluindo o model de Image e User de acordo com os posts
-    ],
-    order: [["createdAt", "DESC"]], // Ordenar os posts pelo campo 'createdAt' em ordem decrescente
-  });
+	const offset = (page - 1) * limit;
+	const retrivedPosts = await Post.findAll({
+		limit,
+		offset,
+		include: [
+			{
+				model: User,
+				where: { deletedAt: null, id },
+			},
+			{ model: Image }, //inscluindo o model de Image e User de acordo com os posts
+		],
+		order: [["createdAt", "DESC"]], // Ordenar os posts pelo campo 'createdAt' em ordem decrescente
+	});
 
-  // Para cada post, obtemos as contagens de likes e dislikes
-  const postsWithLikesAndDislikesAndComment = await Promise.all(
-    retrivedPosts.map(async (post) => {
-      const like = await LikesPost.count({
-        where: { ownerId: post.dataValues.id, type: "like" },
-      });
+	// Para cada post, obtemos as contagens de likes e dislikes
+	const postsWithLikesAndDislikesAndComment = await Promise.all(
+		retrivedPosts.map(async (post) => {
+			const like = await LikesPost.count({
+				where: { ownerId: post.dataValues.id, type: "like" },
+			});
 
-      const dislike = await LikesPost.count({
-        where: { ownerId: post.dataValues.id, type: "dislike" },
-      });
+			const dislike = await LikesPost.count({
+				where: { ownerId: post.dataValues.id, type: "dislike" },
+			});
 
-      const comment = await Comment.count({
-        where: { postId: post.dataValues.id },
-      });
+			const comment = await Comment.count({
+				where: { postId: post.dataValues.id },
+			});
 
-      return {
-        ...post.dataValues,
-        like,
-        dislike,
-        comment,
-      };
-    })
-  );
+			return {
+				...post.dataValues,
+				like,
+				dislike,
+				comment,
+			};
+		})
+	);
 
-  const count = Post.count({ where: { userId: id } });
+	const count = await Post.count({ where: { userId: id } });
 
-  const postUserImageLikesCount = {
-    count,
-    raw: {
-      ...postsWithLikesAndDislikesAndComment,
-    },
-  };
+	const postUserImageLikesCount = {
+		count,
+		raw: postsWithLikesAndDislikesAndComment,
+	};
 
-  return postUserImageLikeSchema.parse(postUserImageLikesCount);
+	return postUserImageLikeSchema.parse(postUserImageLikesCount);
 };
 
 export default getUserAllPostsService;
