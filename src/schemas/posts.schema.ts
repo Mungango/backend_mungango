20107aea20fe475a426b67149cff54cd639e7037
isebelle9912 <isabellesaahirah28@gmail.com>
2024-06-29 17:37:29 -0300

    feat: sevice de all posts user com o count

--- src/schemas/posts.schema.ts
+++ src/schemas/posts.schema.ts
@@ -3,37 +3,40 @@ import { usersWithoutPassSchema } from "./users.schema";
 //import { comments } from "./comments.schema";
 import { uploadSchema } from "./upload.schema";
 import User from "../models/User";
+import { raw } from "express";
 
 const postsSchema = z.object({
-	id: z.number(),
-	message: z.string(),
-	createdAt: z.date(),
-	updatedAt: z.date(),
-	userId: z.number(),
+  id: z.number(),
+  message: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  userId: z.number(),
 });
 
 const postUserImageSchema = postsSchema
-	.extend({
-		Images: uploadSchema.array(),
-		User: usersWithoutPassSchema,
-	})
-	.omit({ userId: true });
+  .extend({
+    Images: uploadSchema.array(),
+    User: usersWithoutPassSchema,
+  })
+  .omit({ userId: true });
 
-const postUserImageLikeSchema = postsSchema
-	.extend({
-		like: z.number(),
-		dislike: z.number(),
-		comment: z.number(),
-		Images: uploadSchema.array(),
-		User: usersWithoutPassSchema,
-	})
-	.omit({ userId: true });
+const postUserImageLikeSchema = z.object({ count: z.number() }).extend({
+  raw: postsSchema
+    .extend({
+      like: z.number(),
+      dislike: z.number(),
+      comment: z.number(),
+      Images: uploadSchema.array(),
+      User: usersWithoutPassSchema,
+    })
+    .array(),
+});
 
 const postsCreateSchema = postsSchema.omit({
-	id: true,
-	userId: true,
-	createdAt: true,
-	updatedAt: true,
+  id: true,
+  userId: true,
+  createdAt: true,
+  updatedAt: true,
 });
 // .extend({
 // 	images: z
@@ -46,9 +49,9 @@ const postsCreateSchema = postsSchema.omit({
 const postsUpdateSchema = postsCreateSchema.partial();
 
 export {
-	postsSchema,
-	postUserImageLikeSchema,
-	postUserImageSchema,
-	postsCreateSchema,
-	postsUpdateSchema,
+  postsSchema,
+  postUserImageLikeSchema,
+  postUserImageSchema,
+  postsCreateSchema,
+  postsUpdateSchema,
 };
--- src/services/posts/getUserAllPosts.service.ts
+++ src/services/posts/getUserAllPosts.service.ts
@@ -6,53 +6,61 @@ import LikesPost from "../../models/likesPost";
 import { postUserImageLikeSchema } from "../../schemas/posts.schema";
 import User from "../../models/User";
 import { iPostUserImageLike } from "../../interfaces/post.interface";
+import { raw } from "express";
 
 const getUserAllPostsService = async (
-	id: number,
-	page: number,
-	limit: number
-): Promise<iPostUserImageLike[]> => {
-	const offset = (page - 1) * limit;
-	const retrivedPosts = await Post.findAll({
-		limit,
-		offset,
-		include: [
-			{
-				model: User,
-				where: { deletedAt: null, id },
-			},
-			{ model: Image }, //inscluindo o model de Image e User de acordo com os posts
-		],
-		order: [["createdAt", "DESC"]], // Ordenar os posts pelo campo 'createdAt' em ordem decrescente
-	});
+  id: number,
+  page: number,
+  limit: number
+): Promise<iPostUserImageLike> => {
+  const offset = (page - 1) * limit;
+  const retrivedPosts = await Post.findAll({
+    limit,
+    offset,
+    include: [
+      {
+        model: User,
+        where: { deletedAt: null, id },
+      },
+      { model: Image }, //inscluindo o model de Image e User de acordo com os posts
+    ],
+    order: [["createdAt", "DESC"]], // Ordenar os posts pelo campo 'createdAt' em ordem decrescente
+  });
 
-	// Para cada post, obtemos as contagens de likes e dislikes
-	const postsWithLikesAndDislikesAndComment = await Promise.all(
-		retrivedPosts.map(async (post) => {
-			const like = await LikesPost.count({
-				where: { ownerId: post.dataValues.id, type: "like" },
-			});
+  // Para cada post, obtemos as contagens de likes e dislikes
+  const postsWithLikesAndDislikesAndComment = await Promise.all(
+    retrivedPosts.map(async (post) => {
+      const like = await LikesPost.count({
+        where: { ownerId: post.dataValues.id, type: "like" },
+      });
 
-			const dislike = await LikesPost.count({
-				where: { ownerId: post.dataValues.id, type: "dislike" },
-			});
+      const dislike = await LikesPost.count({
+        where: { ownerId: post.dataValues.id, type: "dislike" },
+      });
 
-			const comment = await Comment.count({
-				where: { postId: post.dataValues.id },
-			});
+      const comment = await Comment.count({
+        where: { postId: post.dataValues.id },
+      });
 
-			return {
-				...post.dataValues,
-				like,
-				dislike,
-				comment,
-			};
-		})
-	);
+      return {
+        ...post.dataValues,
+        like,
+        dislike,
+        comment,
+      };
+    })
+  );
 
-	return postUserImageLikeSchema
-		.array()
-		.parse(postsWithLikesAndDislikesAndComment);
+  const count = Post.count({ where: { userId: id } });
+
+  const postUserImageLikesCount = {
+    count,
+    raw: {
+      ...postsWithLikesAndDislikesAndComment,
+    },
+  };
+
+  return postUserImageLikeSchema.parse(postUserImageLikesCount);
 };
 
 export default getUserAllPostsService;
